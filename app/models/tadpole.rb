14634e4c5b9fa7808972c8fff99cf77c77c87954
Arthur Torres <arthurtorres002@gmail.com>
2019-01-17 14:16:36 -0500

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,13 @@
 class Tadpole < ActiveRecord::Base
-  # code goes here
+  belongs_to :frog
+  delegate :pond, to: :frog, allow_nil: true
+
+  before_destroy :confirm_frog
+
+  private
+
+  def confirm_frog
+    !Frog.find_by(name: self.name).nil?
+  end
+
 end
